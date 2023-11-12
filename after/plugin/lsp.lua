6918a8ca9ae38413a9b5bd4e8245c7dfea36c1ae
hkder <samhskim1@gmail.com>
2023-11-11 23:34:24 -0800

    fix lua keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,18 @@
 local lsp_zero = require('lsp-zero')
 
-
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
+
+  vim.keymap.set("n", "gd", function() vim.lsp_zero.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp_zero.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp_zero.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp_zero.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp_zero.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp_zero.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp_zero.buf.signature_help() end, opts)
 end)
 
 require('mason').setup({})
@@ -46,21 +56,6 @@ lsp_zero.set_preferences({
         info = 'I'
     }
 })
-
-lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp_zero.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp_zero.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp_zero.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp_zero.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp_zero.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp_zero.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp_zero.buf.signature_help() end, opts)
-end)
 
 lsp_zero.setup()
 
